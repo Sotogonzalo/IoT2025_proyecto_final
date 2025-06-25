faf9f898e4472a805fda424e0a9c0b52b26b9e77
selt2002 <selt2002@gmail.com>
2025-06-25 14:48:20 -0300

    NTP incial, para usar con el logger luego

--- audio_player/components/esp_rom_delay_ms/include/esp_rom_delay_ms.h
+++ audio_player/components/esp_rom_delay_ms/include/esp_rom_delay_ms.h
@@ -1,1 +1,5 @@
+#ifndef ESP_ROM_DELAY_MS
+#define ESP_ROM_DELAY_MS
+
 void esp_rom_delay_ms(int delay);
+#endif
---
+++ audio_player/components/ntp_sync/include/sntp.h
@@ -0,0 +1,8 @@
+#ifndef NTP_SYNC
+#define NTP_SYNC
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+#endif
